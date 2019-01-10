25cf294df58025b57a3724075e88a2b51663746e
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-01-10 17:16:46 +0200

    Sentence Segmentation
    Added general-purpose news or web text Sentence Segmentation

--- ken-server.py
+++ ken-server.py
@@ -3,8 +3,9 @@
 
 from __future__ import unicode_literals
 
-import nltk
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
+from nltk import sent_tokenize
+import spacy
+
 import pickle
 import codecs
 import logging
@@ -17,22 +18,40 @@ from flask import Flask, jsonify, request
 from flask import abort
 app = Flask(__name__)
 
-@app.route('/ken/api/v1.0/en/filesplit', methods=['POST'])
-def fileSplit():
+@app.route('/ken/api/v1.0/en/file/parce/<string:lib_name>', methods=['POST'])
+def file_Sentence_Segmentation(lib_name):
+    if lib_name == "nltk":
+        return file_Sentence_Segmentation_NLTK()
+    if lib_name == "spacy":
+        return file_Sentence_Segmentation_SpaCy()
 
-    yet_raw_text_list = []
-
+def file_Sentence_Segmentation_NLTK():
     try:
-        file = request.files['file']
+        file = request. files['file']
         raw_text = file.read().decode('utf-8')
-        for line in raw_text.splitlines():
-            if re.search('[А-ЯІЄЇҐа-яієїґ]', line):
-                line = line.strip()
-                line = " ".join(line.split())
-                print(line)
-                yet_raw_text_list.append(line)
-        yet_raw_text = '\n'.join(yet_raw_text_list)
-        return yet_raw_text
+        file.close()
+        sentences = sent_tokenize(raw_text)
+        for sentence in sentences:
+            print(sentence + "\n")
+        return sentences[0]
+    except KeyError:
+        return jsonify({"Error": {"KeyError": "One of the words is missing" }})
+
+def file_Sentence_Segmentation_SpaCy():
+    try:
+        file = request. files['file']
+        raw_text = file.read().decode('utf-8')
+        file.close()
+        nlp = spacy.load('en_core_web_sm')
+        doc = nlp(raw_text)
+        print('''
+        sentences\t{num_sent}
+        '''.format(
+        num_sent=len(list(doc.sents)),))
+        for sentence in doc.sents:
+            print("-------")
+            print(sentence)
+        return doc.text
     except KeyError:
         return jsonify({"Error": {"KeyError": "One of the words is missing" }})
 
