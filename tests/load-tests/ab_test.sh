1d363c4c43a77f63a923c92084dd2f4098b3ba0b
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-03-14 14:12:35 +0200

    ab script updates

--- tests/load-tests/ab_test.sh
+++ tests/load-tests/ab_test.sh
@@ -6,4 +6,4 @@
 # -c is concurrent clients
 # -n is the number of requests to run in the test
 
-ab -p test_data.pdf -T 'multipart/form-data; boundary=1234567890' -c 5 -n 6 http://icybcluster.org.ua:35145/api/v1/recap
+ab -p test_data.pdf -T 'multipart/form-data; boundary=1234567890' -c 3 -n 2 http://icybcluster.org.ua:35145/api/v1/recap
