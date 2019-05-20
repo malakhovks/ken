18c4e0d495e45b9f71257cf2426f2642af31cf86
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-05-20 17:06:44 +0300

    add new json structure template

---
+++ resources/tests/scratch-code/new-structure/struct.py
@@ -0,0 +1,39 @@
+allterms = {
+  "termsintext": {
+    "filepath": "/home/recapService/consoles/console_1543823713324/fileForAnalysisDocXCP1251.txt",
+    "exporterms": {
+      "term": [
+        {
+          "ttype": "Noun",
+          "tname": "sdaf",
+          "wcount": "1",
+          "osn": "dsfa",
+          "sentpos": [
+            "0/1",
+            "2/1",
+            "3/1",
+            "5/1",
+            "8/6",
+            "11/13",
+            "12/9",
+            "13/2",
+            "13/51",
+            "14/12",
+            "15/1",
+            "16/13",
+            "17/1",
+            "18/3",
+            "19/1"
+          ]
+        }
+      ]
+    },
+    "sentences": {
+      "sent": [
+        "sdmfasdf sadfasd asdfasdfasdf",
+        "sdfa dsfasdf sdf frefg 454  d sf a dfa sdfas"
+      ]
+    }
+  }
+}
+print(allterms['termsintext']['filepath'])
