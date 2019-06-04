7e1cabc7210fc8f11c5b731e484a6df4646642d4
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-06-04 11:40:40 +0300

    clean

--- resources/tests/scratch-code/visualizing/sentence-render-body-tag.py
+++ resources/tests/scratch-code/visualizing/sentence-render-body-tag.py
@@ -1,24 +1,15 @@
 import spacy
 from spacy import displacy
-# import re, bs4
 
 nlp = spacy.load("en_core_web_sm")
 
-# doc = nlp(u"Google is leading company in the natural language processing")
-doc = nlp(u"After the vision of the Semantic Web was broadcasted at the turn of the millennium, ontology became a synonym for the solution to many problems concerning the fact that computers do not understand human language: if there were an ontology and every document were marked up with it and we had agents that would understand the mark-up, then computers would finally be able to process our queries in a really sophisticated way.")
+doc = nlp(u"Google is leading company in the natural language processing")
 
-# html = displacy.render(doc, style="ent", page=True)
-
-# print(re.findall('<body>(.*?)</body>', html, re.DOTALL))
-# print ('----------------------------------------------------------------------')
-# print(bs4.BeautifulSoup(html).find('body'))
-# print ('----------------------------------------------------------------------')
-# print(html)
 print ('----------------------------------------------------------------------')
-
-colors = {"ORG": "linear-gradient(90deg, #aa9cfc, #fc9ce7)"}
-options = {"ents": ["ORG"], "colors": colors}
+colors = {"ORG": "linear-gradient(90deg, #b0fb5a, #ffffff)"}
+options = {"colors": colors}
 
 svg = displacy.render(doc, style="ent", options=options)
 
+# svg = displacy.render(doc, style="ent")
 print svg
