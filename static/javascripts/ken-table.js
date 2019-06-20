b19acaadfef60651602b5168998c3a190ee956cf
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-06-20 20:26:34 +0300

    cleanup

--- static/javascripts/ken-table.js
+++ static/javascripts/ken-table.js
@@ -209,7 +209,6 @@ function save_table_to_csv(html, filename) {
 
 $('#saveTable').click(function () {
     var html = document.querySelector("table").outerHTML;
-    // export_table_to_csv(html, "table.csv");
     save_table_to_csv(html, "table-for-confor-" + getFormattedTime() + ".csv");
 });
 //SAVE_TABLE_TO_CSV_____________________________________________________________________________________________________
