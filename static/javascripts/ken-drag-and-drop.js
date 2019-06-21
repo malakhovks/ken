aea5ee6d206924591595c3bb7e462cb8da544336
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-06-21 13:28:05 +0300

    clean up
    clean up

--- static/javascripts/ken-drag-and-drop.js
+++ static/javascripts/ken-drag-and-drop.js
@@ -2,19 +2,6 @@
 $('#uploadResultList').on('mousedown', 'option', clickDragAndDrop);
 $('#uploadUnknownTerms').on('mousedown', 'option', clickDragAndDrop);
 
-/* function clickDragAndDrop() {
-    var termText = $(this).prop('value'); // extract text from term-list
-    console.log(termText);
-// Mouse handler for table (DROP):
-    $('#term-table').on('mouseup', 'input', function () {
-        if (termText != '') {
-            $(this).prop('value', termText);
-            localStorage[$(this).prop('id')] = termText;
-            termText = '';
-        }
-    });
-} */
-
 function clickDragAndDrop() {
     var termText = $(this).prop('value'); // extract text from term-list
     console.log(termText);
