d8c5089d601759353b21609090bdce7f8c442688
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-06-21 13:59:43 +0300

    clean up

--- static/javascripts/ken-table.js
+++ static/javascripts/ken-table.js
@@ -404,3 +404,19 @@ $('#addRowsToTable').click(function () {
 $('#addColsToTable').click(function () {
     addColumn(parseInt(0), "right");
 });
+
+// Mouse handler for select-list with terms (DRAG):
+$('#uploadResultList').on('mousedown', 'option', clickDragAndDrop);
+$('#uploadUnknownTerms').on('mousedown', 'option', clickDragAndDrop);
+
+function clickDragAndDrop() {
+    var termText = $(this).prop('value'); // extract text from term-list
+    console.log(termText);
+// Mouse handler for table (DROP):
+    $('#table-body').on('mouseup', 'td', function () {
+        if (termText != '') {
+            $(this).html(termText);
+            termText = '';
+        }
+    });
+}
