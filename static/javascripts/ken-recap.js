cf42d58be5791a5de76361efad9a67c2fd104e9d
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-08-14 12:37:46 +0300

    Set up json temlpate for project

--- static/javascripts/ken-recap.js
+++ static/javascripts/ken-recap.js
@@ -31,7 +31,7 @@ var $buttonNewProjectAndClearAll = $('#button-new-project'),
     $sents_from_text = $('#sents_from_text'),
     $sortSelect = $('#sort-select');
 
-var forProject = { project: [] };
+var forProject = { project: { name: "", content: { files: [] } } };
 /*\
 |*|  Base64 / binary data / UTF-8 strings utilities (#3)
 |*|  https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
