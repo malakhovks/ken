e0db6d20b356ae936c992cbe871c13ea330755cb
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-12-19 16:35:50 +0200

    Update with xvfb
    Update with xvfb

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 # turn on bash's job control
 set -m
 
-{ service nginx start; uwsgi --ini ./deploy/uwsgi.ini; } &
+# { service nginx start; uwsgi --ini ./deploy/uwsgi.ini; } &
+{ service nginx start; uwsgi --ini ./deploy/uwsgi.ini; Xvfb :1; } &
 
-cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 xvfb-run wine Konspekt.exe 1.txt 1 5000
+cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 DISPLAY=:1 wine Konspekt.exe 1.txt 1 5000
 # cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 wine Konspekt.exe 1.txt 1 5000
