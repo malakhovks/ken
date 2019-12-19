b795a01e541d1027820131dc0e08a36b2582200f
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-12-19 15:33:10 +0200

    Update with xvfb
    Update with xvfb

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ set -m
 
 { service nginx start; uwsgi --ini ./deploy/uwsgi.ini; } &
 
-# cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 xvfb-run wine Konspekt.exe 1.txt 1 5000
-cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 wine Konspekt.exe 1.txt 1 5000
+cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 xvfb-run wine Konspekt.exe 1.txt 1 5000
+# cd ./deploy/konspekt && env LC_ALL=ru_RU.CP1251 wine Konspekt.exe 1.txt 1 5000
