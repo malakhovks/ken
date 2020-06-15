d04e37943888b01a9be2904b4b32cf24f2b96464
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-06-15 11:19:28 +0300

    Update animations and trasitions
    Update animations and trasitions

--- static/javascripts/gr.js
+++ static/javascripts/gr.js
@@ -4,9 +4,10 @@ const granimInstance = new Granim({
     elToSetClassOn: '.canvas-interactive-wrapper',
     direction: 'left-right',
     isPausedWhenNotInView: true,
-    stateTransitionSpeed: 300,
+    stateTransitionSpeed: 1000,
     states : {
         "default-state": {
+            transitionSpeed: 2000,
             gradients: [
                 // ['#ff9966', '#ff5e62'],
                 // ['#00F260', '#0575E6'],
